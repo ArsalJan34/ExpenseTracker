22e67cf2f0687498e87df18ed560c2e7b51feda9
ArsalJan34 <arsaljaan532@gmail.com>
2026-06-08 13:20:16 +0500

    Fix Vite base path

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/expense-tracker/"   // ← Add this line (use your exact repo name)
+  base: '/ExpenseTracker/',
 })
